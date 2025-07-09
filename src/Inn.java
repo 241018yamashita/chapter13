17f5fac7cb80ff637e539f9eb5c35cbbead63dbe
YamashitaRuna <241018@std.hi-joho.ac.jp>
2025-07-09 09:50:02 +0900

    13章課題

--- src/Inn.java
+++ src/Inn.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Inn {
+    public void checkIn(Hero h) {
+        System.out.println("宿屋で回復中...");
+
+        int hp = h.getHp();
+        h.setHP(100);
+        System.out.println(h.getName() + "の現在のHP: " + h.getHp());
+        h.sleep();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+
+        Hero h = new Hero();
+        System.out.println(h.getName() + "の現在のHP: " + h.getHp());
+        King k = new King();
+        k.talk(h);
+        Inn i = new Inn();
+        i.checkIn(h);
+        h.attack(new Matango());
+    }
 }
